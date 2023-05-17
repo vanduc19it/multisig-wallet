bbcdd97234e382e8e1cc1d67e0e529ea4386d744
vanduc19it <duc19it@gmail.com>
2023-05-18 01:43:33 +0700

    update smc

--- backend/contracts/Factory.sol
+++ backend/contracts/Factory.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol"; 
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
+//This smc using deploy and manage multisig wallets. use avalable model multisig wallet to deploy new wallet.
 contract MultisigFactory {
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    address public owner;
-    address public implementation;
-    mapping(address => EnumerableSet.AddressSet) private deployments;
+    address public owner; // owner wallet
+    address public implementation; // store available smc model.
+    mapping(address => EnumerableSet.AddressSet) private deployments; // track deployed Multisig contract addresses by each deployer.
 
     event ImplementationUpdated(address _caller, address _implementation);
     event ContractDeployed(address indexed _deployer, address _deployedContract, address _implementation);
@@ -19,25 +20,32 @@ contract MultisigFactory {
         implementation = _implementation;
     }
 
+    // update address of template smc multisig
     function setImplementation(address _implementation) public {
         require(msg.sender == owner, "Not owner!");
         implementation = _implementation;
         emit ImplementationUpdated(msg.sender, _implementation);
     }
 
+    //deploy a new contract multisig wallet 
     function deployContract(bytes memory _data) public {
+        //using clone.clone to create duplicate of template
         address deployedContract = Clones.clone(implementation);
+        //call data and check if success
         (bool success, ) = deployedContract.call(_data);
         require(success, "Failed to initialize contract!");
+        //add deployed contratc to deployment of msg.sender and check
         bool added = deployments[msg.sender].add(deployedContract);
         require(added, "Failed to add to registry!");
         emit ContractDeployed(msg.sender, deployedContract, implementation);
     }
 
+    //return an array of address contract multisig deployed by deployeer
     function getDeployed(address _deployer) public view returns(address[] memory) {
         return deployments[_deployer].values();
     }
 
+    //return amount of contract multisig deployed by deployer
     function countDeployed(address _deployer) public view returns(uint256) {
         return deployments[_deployer].length();
     }
--- backend/contracts/Multisig.sol
+++ backend/contracts/Multisig.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
 contract MultisigWallet is Initializable {
-    uint256 private _requiredSignatures;
-    address[] private _owners;
+    uint256 private _requiredSignatures; // The number of signatures required to execute the transaction.
+    address[] private _owners; //aray of owners address
 
     struct Transaction {
         address to;
@@ -12,29 +12,33 @@ contract MultisigWallet is Initializable {
         bytes data;
         bool executed;
         mapping(address => bool) signatures;
+        uint numConfirmations;
     }
 
-    Transaction[] private _transactions;
+    Transaction[] private _transactions; //array of transactions
 
     event TransactionCreated(uint256 transactionId, address to, uint256 value, bytes data);
     event TransactionSigned(uint256 indexed transactionId, address indexed signer);
     event TransactionExecuted(uint256 transactionId, address executer);
+    event RevokeConfirmation(address indexed owner, uint indexed transactionId);
 
+    //used to initialize the contract and set the initial parameters such as list owners and required number of signatures
     function initialize(address[] memory owners, uint256 requiredSignatures) public initializer {
         require(owners.length > 0, "At least one owner required");
         require(requiredSignatures > 0 && requiredSignatures <= owners.length, "Invalid number of required signatures");
-
         _owners = owners;
         _requiredSignatures = requiredSignatures;
     }
 
+    //use to create new transaction
     function submitTransaction(address to, uint256 value, bytes memory data) public {
         require(isOwner(msg.sender), "Not an owner!");
         require(to != address(0), "Invalid destination address");
         require(value >= 0, "Invalid value");
 
         uint256 transactionId = _transactions.length;
-        _transactions.push();
+        _transactions.push(); //push transaction to array _transactions
+        // Assign newly created transaction to variable transaction
         Transaction storage transaction = _transactions[transactionId];
         transaction.to = to;
         transaction.value = value;
@@ -44,13 +48,14 @@ contract MultisigWallet is Initializable {
         emit TransactionCreated(transactionId, to, value, data);
     }
 
+    //use to confirm transaction by owners
     function signTransaction(uint256 transactionId) public {
         require(transactionId < _transactions.length, "Invalid transaction ID");
         Transaction storage transaction = _transactions[transactionId];
         require(!transaction.executed, "Transaction already executed");
         require(isOwner(msg.sender), "Only owners can sign transactions");
         require(!transaction.signatures[msg.sender], "Transaction already signed by this owner");
-
+        transaction.numConfirmations += 1;
         transaction.signatures[msg.sender] = true;
         emit TransactionSigned(transactionId, msg.sender);
         if(countSignatures(transaction) == _requiredSignatures) {
@@ -58,20 +63,34 @@ contract MultisigWallet is Initializable {
         }
     }
 
+    //using execute transactions after confirm by owners
     function executeTransaction(uint256 transactionId) private {
         require(transactionId < _transactions.length, "Invalid transaction ID");
         Transaction storage transaction = _transactions[transactionId];
         require(!transaction.executed, "Transaction already executed");
         require(countSignatures(transaction) >= _requiredSignatures, "Insufficient valid signatures");
-
         transaction.executed = true;
         (bool success,) = transaction.to.call{value: transaction.value}(transaction.data);
         require(success, "Transaction execution failed");
         emit TransactionExecuted(transactionId, msg.sender);
     }
 
-    // HELPERS
+    //cancel confirm transaction
+    function revokeConfirmation(uint256 transactionId) public {
+        require(isOwner(msg.sender), "Not owner");
+        require(transactionId < _transactions.length, "Transaction does not exist");
+        require(!_transactions[transactionId].executed, "Transaction already executed");
 
+        Transaction storage transaction = _transactions[transactionId];
+
+        require(transaction.signatures[msg.sender], "Transaction not confirmed");
+
+        transaction.numConfirmations -= 1;
+        transaction.signatures[msg.sender] = false;
+
+        emit RevokeConfirmation(msg.sender,transactionId);
+    }
+    //check if owner
     function isOwner(address account) public view returns (bool) {
         for (uint256 i = 0; i < _owners.length; i++) {
             if (_owners[i] == account) {
@@ -80,7 +99,7 @@ contract MultisigWallet is Initializable {
         }
         return false;
     }
-
+    //return amount of signatures
     function countSignatures(Transaction storage transaction) private view returns (uint256) {
         uint256 count = 0;
         for (uint256 i = 0; i < _owners.length; i++) {
@@ -91,20 +110,25 @@ contract MultisigWallet is Initializable {
         return count;
     }
 
+    //get and return transaction detail
     function getTransaction(uint256 transactionId) public view returns (address, uint256, bytes memory, bool, uint256) {
         require(transactionId < _transactions.length, "Invalid transaction ID");
 
         Transaction storage transaction = _transactions[transactionId];
         return (transaction.to, transaction.value, transaction.data, transaction.executed, countSignatures(transaction));
     }
-
+    //return amount of transaction
+    function getTransactionCount() public view returns (uint) {
+        return _transactions.length;
+    }
+    //get array of owners
     function getOwners() public view returns(address[] memory) {
         return _owners;
     }
-
+    //return amount of signature nesscessary to excute transaction
     function getRequiredSignatures() public view returns(uint256) {
         return _requiredSignatures;
     }
 
-    receive() external payable {}
+    receive() external payable {} //receive eth from other address tranfer to this multisig wallet address
 }
